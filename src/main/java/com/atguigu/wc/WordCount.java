d1f3ed93c2f66ed0532d02fc4048d1a400c68af6
masteryang4 <2692474773@qq.com>
2020-12-17 18:11:23 +0800

    'firstcommit'

---
+++ src/main/java/com/atguigu/wc/WordCount.java
@@ -0,0 +1,30 @@
+package com.atguigu.wc;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.AggregateOperator;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+public class WordCount {
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSet<String> inputData = env.readTextFile("D:\\develop\\FlinkTutorial\\src\\main\\resources\\text.txt");
+
+        AggregateOperator<Tuple2<String, Integer>> sum = inputData.flatMap(new myFlatMapFunction()).groupBy(0).sum(1);
+
+        sum.print();
+    }
+
+    public static class myFlatMapFunction implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
+            String[] words = s.split(" ");
+            for (String word : words) {
+                collector.collect(new Tuple2<>(word, 1));
+            }
+        }
+    }
+}
