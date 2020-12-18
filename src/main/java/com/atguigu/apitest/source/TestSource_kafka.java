e49e143965f16b66666f7eb3310eeba0c13c8cd5
masteryang4 <2692474773@qq.com>
2020-12-18 20:58:59 +0800

    'kafka-flink'

---
+++ src/main/java/com/atguigu/apitest/source/TestSource_kafka.java
@@ -0,0 +1,29 @@
+package com.atguigu.apitest.source;
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+
+import java.util.Properties;
+
+public class TestSource_kafka {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        Properties properties = new Properties();
+
+        properties.setProperty("bootstrap.servers", "hadoop102:9092");
+        properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("auto.offset.reset", "latest");
+
+        DataStream<String> dataStream = env.addSource(new FlinkKafkaConsumer011<String>("myflink-kafka", new SimpleStringSchema(), properties));
+
+        dataStream.print();
+
+        env.execute();
+    }
+}
