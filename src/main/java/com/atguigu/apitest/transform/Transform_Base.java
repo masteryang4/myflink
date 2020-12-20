09e95c31dfff3fcf86a156112bd8abf437a5672c
masteryang4 <2692474773@qq.com>
2020-12-20 16:16:05 +0800

    reduce

--- src/main/java/com/atguigu/apitest/transform/Transform_Base.java
+++ src/main/java/com/atguigu/apitest/transform/Transform_Base.java
@@ -2,6 +2,7 @@ package com.atguigu.apitest.transform;
 
 import com.atguigu.apitest.beans.SensorReading;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
@@ -24,11 +25,22 @@ public class Transform_Base {
                 return new SensorReading(strings[0], new Long(strings[1]), new Double(strings[2]));
             }
         });
-        KeyedStream<SensorReading, Tuple> id = dataStream.keyBy("id"); //【注意返回类型】
+        KeyedStream<SensorReading, Tuple> id = dataStream.keyBy("id"); //【注意返回类型是tuple，keyby不会对类型做出改变】
 
         SingleOutputStreamOperator<SensorReading> temperature = id.maxBy("temperature");
 
-        temperature.print();
+        //**************************************//
+
+        SingleOutputStreamOperator<SensorReading> reduce = id.reduce(new ReduceFunction<SensorReading>() {
+            @Override
+            public SensorReading reduce(SensorReading sensorReading, SensorReading t1) throws Exception {
+                return new SensorReading(sensorReading.getId(), t1.getTimestamp(), Math.max(sensorReading.getTemperature(), t1.getTemperature()));
+            }
+        });
+
+        temperature.print("temperature");
+
+        reduce.print("reduce");
 
 //        id.print();
         env.execute();
