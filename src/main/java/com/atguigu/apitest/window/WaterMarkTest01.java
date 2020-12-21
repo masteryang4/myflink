03c4c55786d207af01d77a7d8e2bd54163f8fc05
masteryang4 <2692474773@qq.com>
2020-12-21 16:23:36 +0800

    quick-sort

--- src/main/java/com/atguigu/apitest/window/WaterMarkTest01.java
+++ src/main/java/com/atguigu/apitest/window/WaterMarkTest01.java
@@ -6,6 +6,14 @@ import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
+import org.apache.flink.streaming.api.functions.AssignerWithPunctuatedWatermarks;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.watermark.Watermark;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+import javax.annotation.Nullable;
 
 public class WaterMarkTest01 {
     public static void main(String[] args) throws Exception {
@@ -23,6 +31,23 @@ public class WaterMarkTest01 {
                 return new SensorReading(ss[0], new Long(ss[1]), new Double(ss[2]));
             }
         });
+
+        //assignTimestampsAndWatermarks 分配时间戳和水位线，有两种：（1）周期型分配水位线（2）断点式分配水位线
+        //默认是周期性生成水位线
+        SingleOutputStreamOperator<SensorReading> m1 = map.assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<SensorReading>(Time.seconds(1)) {
+            @Override
+            public long extractTimestamp(SensorReading sensorReading) {
+                return sensorReading.getTimestamp() * 1000L;
+            }
+        });
+
+        SingleOutputStreamOperator<SensorReading> m2 = map.assignTimestampsAndWatermarks(new AscendingTimestampExtractor<SensorReading>() {
+            @Override
+            public long extractAscendingTimestamp(SensorReading sensorReading) {
+                return sensorReading.getTimestamp() * 1000L;
+            }
+        });
+
 
         map.print();
 
--- src/main/java/com/ys/algo/QuickSort.java
+++ src/main/java/com/ys/algo/QuickSort.java
@@ -1,4 +1,54 @@
 package com.ys.algo;
 
+import java.util.Arrays;
+
+/**
+ * 快速排序
+ * 双指针法 + 递归
+ */
+
 public class QuickSort {
+
+    private static void quickSort(int[] a, int left, int right) {
+//        if (right < left) { //【重点】
+//            return;
+//        }
+        int temp = left;
+        int l = left;
+        int r = right;
+        int t = 0;
+        while (right > left) {
+            while (right > left && a[right] > a[temp]) {
+                right--;
+            }
+            while (right > left && a[left] < a[temp]) {
+                left++;
+            }
+            t = a[right];
+            a[right] = a[left];
+            a[left] = t;
+        }
+
+        if (right == left) {
+            t = a[right];
+            a[right] = a[temp];
+            a[temp] = t;
+        } else {
+            return;  //【重点】
+        }
+
+        quickSort(a, l, temp - 1);
+        quickSort(a, temp + 1, r);
+
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 1, 5, 4, 8, 6, 7, 100, 13, 26, 15};
+        System.out.println(Arrays.toString(a));
+
+        quickSort(a, 0, a.length - 1);
+
+        System.out.println(Arrays.toString(a));
+    }
+
 }
