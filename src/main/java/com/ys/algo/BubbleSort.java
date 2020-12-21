0f9711f38ec823826b68d5e4d67f571abc6b3c2f
masteryang4 <2692474773@qq.com>
2020-12-21 13:31:16 +0800

    algo-bubbleSort

--- src/main/java/com/ys/algo/BubbleSort.java
+++ src/main/java/com/ys/algo/BubbleSort.java
@@ -1,4 +1,32 @@
 package com.ys.algo;
 
+import java.util.Arrays;
+
+/**
+ * 冒泡排序
+ * 双层for循环
+ */
 public class BubbleSort {
+
+    public static void bubbleSort(int[] a) {
+        int temp = 0;
+        for (int i = 1; i < a.length; i++) {
+            for (int j = 0; j < a.length - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 1, 5, 4, 8, 6, 7, 100, 13, 26, 15};
+        System.out.println(Arrays.toString(a));
+
+        bubbleSort(a);
+
+        System.out.println(Arrays.toString(a));
+    }
 }
