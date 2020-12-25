cf602547889505ff15be95505448b471f0f905fe
masteryang4 <2692474773@qq.com>
2020-12-26 01:03:17 +0800

    statebackend

--- src/main/java/com/atguigu/apitest/state/OperatorStateTest.java
+++ src/main/java/com/atguigu/apitest/state/OperatorStateTest.java
@@ -1,6 +1,8 @@
 package com.atguigu.apitest.state;
 
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.runtime.state.memory.MemoryStateBackend;
 import org.apache.flink.streaming.api.checkpoint.ListCheckpointed;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
@@ -13,6 +15,10 @@ public class OperatorStateTest {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
+
+//        env.setStateBackend(new MemoryStateBackend());
+//        env.setStateBackend(new FsStateBackend(""));
+
         DataStreamSource<String> hadoop102 = env.socketTextStream("hadoop102", 7777);
         SingleOutputStreamOperator<Integer> map = hadoop102.map(new myMapFunc());
 
