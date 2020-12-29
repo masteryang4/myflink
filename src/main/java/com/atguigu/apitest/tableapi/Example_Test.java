5b746801c5247a62a41684bf29bc37b374db99be
masteryang4 <2692474773@qq.com>
2020-12-29 16:48:42 +0800

    tableapi-sql

---
+++ src/main/java/com/atguigu/apitest/tableapi/Example_Test.java
@@ -0,0 +1,43 @@
+package com.atguigu.apitest.tableapi;
+
+import com.atguigu.apitest.beans.SensorReading;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
+
+public class Example_Test {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        DataStreamSource<String> ds = env.readTextFile("D:\\develop\\FlinkTutorial\\src\\main\\resources\\sensor.txt");
+
+        SingleOutputStreamOperator<SensorReading> map = ds.map(new MapFunction<String, SensorReading>() {
+            @Override
+            public SensorReading map(String s) throws Exception {
+                String[] strings = s.split(",");
+                return new SensorReading(strings[0], new Long(strings[1]), new Double(strings[2]));
+            }
+        });
+
+        //tableapi
+        StreamTableEnvironment streamTableEnvironment = StreamTableEnvironment.create(env);//【核心】
+
+        Table table = streamTableEnvironment.fromDataStream(map);
+        Table tableapi_result = table.select("id,temperature").where("id='sensor_1'");
+//        tableapi_result.printSchema();
+        streamTableEnvironment.toAppendStream(tableapi_result, Row.class).print("tableapi");
+
+        //sql
+        streamTableEnvironment.createTemporaryView("sensor", map);
+        String sqlstring = "select id,temperature from sensor where id='sensor_1'";
+        Table table1 = streamTableEnvironment.sqlQuery(sqlstring);
+        streamTableEnvironment.toAppendStream(table1, Row.class).print("sql");
+
+        env.execute();
+    }
+}
