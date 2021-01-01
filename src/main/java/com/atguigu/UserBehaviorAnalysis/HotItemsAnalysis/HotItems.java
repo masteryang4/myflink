f2e2a70c68aceb7e724132717973f36059cdafcf
masteryang4 <2692474773@qq.com>
2021-01-01 21:54:22 +0800

    hotitems1

--- src/main/java/com/atguigu/UserBehaviorAnalysis/HotItemsAnalysis/HotItems.java
+++ src/main/java/com/atguigu/UserBehaviorAnalysis/HotItemsAnalysis/HotItems.java
@@ -1,17 +1,24 @@
 package com.atguigu.UserBehaviorAnalysis.HotItemsAnalysis;
 
+import com.atguigu.UserBehaviorAnalysis.HotItemsAnalysis.beans.ItemViewCount;
 import com.atguigu.UserBehaviorAnalysis.HotItemsAnalysis.beans.UserBehavior;
+import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+import scala.collection.Iterable;
 
 public class HotItems {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
@@ -37,7 +44,43 @@ public class HotItems {
                 return userBehavior.getBehavior() == "pv" ? true : false;
             }
         }).keyBy("itemId")
-                .timeWindow(Time.hours(1),Time.minutes(5))
-                .aggregate()
+                .timeWindow(Time.hours(1), Time.minutes(5))
+                .aggregate(new MyAggregateFunction(), new MyWindowFuncion());
+
+    }
+
+    public static class MyAggregateFunction implements AggregateFunction<UserBehavior, Long, Long> {
+
+        @Override
+        public Long createAccumulator() {
+            return 0L;
+        }
+
+        @Override
+        public Long add(UserBehavior userBehavior, Long aLong) {
+            return aLong + 1;
+        }
+
+        @Override
+        public Long getResult(Long aLong) {
+            return aLong;
+        }
+
+        @Override
+        public Long merge(Long aLong, Long acc1) {
+            return aLong + acc1;
+        }
+    }
+
+    public static class MyWindowFuncion implements WindowFunction<Long, ItemViewCount, Tuple, TimeWindow> {
+
+        @Override
+        public void apply(Tuple tuple, TimeWindow window, java.lang.Iterable<Long> input, Collector<ItemViewCount> out) throws Exception {
+            Long itemId = tuple.getField(0);
+            Long count = input.iterator().next();
+            long windowEnd = window.getEnd();
+
+            out.collect(new ItemViewCount(itemId, windowEnd, count));
+        }
     }
 }
