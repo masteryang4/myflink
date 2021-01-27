f9abce2a87d5d5e06c5c45374128892f5c5a691a
masteryang4 <2692474773@qq.com>
2021-01-27 20:54:38 +0800

    algo01-quciksort01

---
+++ src/main/java/com/ys/algo01/SortQuick.java
@@ -0,0 +1,43 @@
+package com.ys.algo01;
+
+import java.util.Arrays;
+
+public class SortQuick {
+    public static void quick_sort(int[] arr, int left, int right) {
+        if (left > right) {
+            return;
+        }
+
+        int flag = arr[left];
+        int i = left;
+        int j = right;
+        int tmp = 0;
+        while (i < j) {
+            while (arr[j] >= flag && i < j) { //【注意】条件二也很重要，缺一不可
+                j--;
+            }
+            while (arr[i] <= flag && i < j) {
+                i++;
+            }
+            tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+        if (i == j) {
+            arr[left] = arr[i];
+            arr[i] = flag;
+        }
+
+        quick_sort(arr, left, i - 1);
+        quick_sort(arr, i + 1, right);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {6, 1, 2, 7, 9};
+        System.out.println(Arrays.toString(arr));
+
+        quick_sort(arr, 0, arr.length - 1);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
