254df52a4b5cff28f814c01e1a80e82e58917028
masteryang4 <2692474773@qq.com>
2021-01-28 20:24:25 +0800

    spark-cityTest-hivesql-shujuqx

--- src/main/java/com/ys/myspark_test/cityTest.sql
+++ src/main/java/com/ys/myspark_test/cityTest.sql
@@ -23,6 +23,18 @@ from (
 group by 10_city
 ;
 
+--[常用方法]开启参数，两阶段MR，解决group by数据倾斜
+set hive.groupby.skewindata = true;
+/*
+当 sql 语句使用 groupby 时数据出现倾斜时，如果该变量设置为 true，那么 Hive 会自动进行 负载均衡。
+策略就是把 MR 任务拆分成两个：第一个先做预汇总，第二个再做最终汇总
+
+在 MR 的第一个阶段中，Map 的输出结果集合会缓存到 maptaks 中，每个 Reduce 做部分聚合操作，并输出结果，
+这样处理的结果是相同 Group By Key 有可能被分发到不同的 Reduce 中， 从而达到负载均衡的目的；
+第二个阶段 再根据预处理的数据结果按照 Group By Key 分布到 Reduce 中
+（这个过程可以保证相同的 Group By Key 被分布到同一个 Reduce 中），最后完成 最终的聚合操作。
+*/
+
 --uv-----------
 select city, count(distinct userid)
 from t_city
