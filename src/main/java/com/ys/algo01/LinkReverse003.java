c36bd89c804ee5aab775037beda7537a3b45d6dc
masteryang4 <2692474773@qq.com>
2021-02-07 17:06:37 +0800

    reverlink-practice-alldigui

--- src/main/java/com/ys/algo01/LinkReverse003.java
+++ src/main/java/com/ys/algo01/LinkReverse003.java
@@ -16,8 +16,20 @@ public class LinkReverse003 {
         return pre;
     }
 
+    private static LinkofMine reverse_digui(LinkofMine node) {
+        if (node.next == null) { //递归返回条件
+            return node;
+        }
+        LinkofMine last = reverse_digui(node.next); //递归
+        node.next.next = node;
+        node.next = null;
+        return last;
+    }
+
     public static void main(String[] args) {
-        LinkofMine l4 = new LinkofMine(4, null);
+        LinkofMine l6 = new LinkofMine(6, null);
+        LinkofMine l5 = new LinkofMine(5, l6);
+        LinkofMine l4 = new LinkofMine(4, l5);
         LinkofMine l3 = new LinkofMine(3, l4);
         LinkofMine l2 = new LinkofMine(2, l3);
         LinkofMine l1 = new LinkofMine(1, l2);
@@ -29,7 +41,10 @@ public class LinkReverse003 {
         }
         System.out.println("==============>>>");
 
-        next = reverse_diedai(l1);
+//        next = reverse_diedai(l1);
+
+        next = reverse_digui(l1);
+
         while (next != null) {
             System.out.println(next.node);
             next = next.next;
