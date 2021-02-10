d2bde1a6635e9d61530c875a17e9e934c1b8ebbd
masteryang4 <2692474773@qq.com>
2021-02-10 16:26:16 +0800

    reverlink-practice-kgroup001

---
+++ src/main/java/com/ys/algo01/LinkReverseKgroup001.java
@@ -0,0 +1,63 @@
+package com.ys.algo01;
+
+public class LinkReverseKgroup001 {
+
+    private static LinkofMine reverse_ab(LinkofMine a, LinkofMine b) {
+        LinkofMine pre = null;
+        LinkofMine curr = a;
+        LinkofMine nxt = null;
+        while (curr != b) {
+            nxt = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = nxt;
+        }
+        return pre;
+    }
+
+    private static LinkofMine reverse_kgroup(LinkofMine head, int k) {
+        if (head == null) {
+            return null;
+        }
+        LinkofMine a = head;
+        LinkofMine b = head;
+        for (int i = 0; i < k; i++) {
+            if (b == null) {
+                return head;
+            }
+            b = b.next;
+        }
+        LinkofMine newHead = reverse_ab(a, b);
+        head.next = reverse_kgroup(b, k);
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        LinkofMine l6 = new LinkofMine(6, null);
+        LinkofMine l5 = new LinkofMine(5, l6);
+        LinkofMine l4 = new LinkofMine(4, l5);
+        LinkofMine l3 = new LinkofMine(3, l4);
+        LinkofMine l2 = new LinkofMine(2, l3);
+        LinkofMine l1 = new LinkofMine(1, l2);
+
+        LinkofMine next = l1;
+        while (next != null) {
+            System.out.println(next.node);
+            next = next.next;
+        }
+        System.out.println("==============>>>");
+
+//        next = reverse_ab(l2, l5); //test  432
+
+        next = reverse_kgroup(l1, 2);  // 21 43 65
+
+//        next = reverse_kgroup(l1, 3);  // 321 654
+
+//        next = reverse_kgroup(l1, 4);  // 4321 56
+
+        while (next != null) {
+            System.out.println(next.node);
+            next = next.next;
+        }
+    }
+}
