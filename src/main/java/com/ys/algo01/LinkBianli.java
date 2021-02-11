1fa012a9193e735e9ee4b649ee80cc1e7e25b9c5
masteryang4 <2692474773@qq.com>
2021-02-11 13:58:00 +0800

    reverse-bianli-link

---
+++ src/main/java/com/ys/algo01/LinkBianli.java
@@ -0,0 +1,34 @@
+package com.ys.algo01;
+
+/**
+ * 单链表反向遍历（递归）
+ * 正向遍历（迭代）
+ */
+public class LinkBianli {
+
+    private static void bianli_reverse(LinkofMine l1) {
+        if (l1 == null) {
+            return;
+        }
+        bianli_reverse(l1.next);
+        System.out.println(l1.node);
+    }
+
+    public static void main(String[] args) {
+        LinkofMine l6 = new LinkofMine(6, null);
+        LinkofMine l5 = new LinkofMine(5, l6);
+        LinkofMine l4 = new LinkofMine(4, l5);
+        LinkofMine l3 = new LinkofMine(3, l4);
+        LinkofMine l2 = new LinkofMine(2, l3);
+        LinkofMine l1 = new LinkofMine(1, l2);
+
+        LinkofMine next = l1;
+        while (next != null) {
+            System.out.println(next.node);
+            next = next.next;
+        }
+        System.out.println("==============>>>");
+
+        bianli_reverse(l1);
+    }
+}
