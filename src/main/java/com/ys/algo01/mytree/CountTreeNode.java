9b760b36192cfcad13b97e9a353eef6f767c0af9
masteryang4 <2692474773@qq.com>
2021-02-13 16:13:41 +0800

    countTreeNode

---
+++ src/main/java/com/ys/algo01/mytree/CountTreeNode.java
@@ -0,0 +1,33 @@
+package com.ys.algo01.mytree;
+
+/**
+ * 计算二叉树节点个数（递归）
+ * 【递归思维】
+ */
+public class CountTreeNode {
+
+    private static int count_node(MyTree root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + count_node(root.left) + count_node(root.right);
+    }
+
+    public static void main(String[] args) {
+        MyTree t1 = new MyTree(1);
+        MyTree t2 = new MyTree(2);
+        MyTree t3 = new MyTree(3);
+        MyTree t4 = new MyTree(4);
+        MyTree t5 = new MyTree(5);
+        MyTree t6 = new MyTree(6);
+
+        t1.left = t2;
+        t1.right = t3;
+        t2.left = t4;
+        t2.right = t5;
+        t3.left = t6;
+        MyTree root = t1;
+
+        System.out.println(count_node(root));
+    }
+}
