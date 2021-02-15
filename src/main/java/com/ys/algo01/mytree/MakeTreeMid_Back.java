6d721826f5466ee61783200e0ef54f5852167ba5
masteryang4 <2692474773@qq.com>
2021-02-15 19:54:34 +0800

    MakeTreeMidandBack

--- src/main/java/com/ys/algo01/mytree/MakeTreeMid_Back.java
+++ src/main/java/com/ys/algo01/mytree/MakeTreeMid_Back.java
@@ -30,12 +30,25 @@ public class MakeTreeMid_Back {
     }
 
     private static MyTree build(int[] midarr, int mleft, int mright, int[] backarr, int bleft, int bright) {
-        if(mleft>mright || bleft>bright){
+        if (mleft > mright || bleft > bright) {
             return null;
         }
+        int rootval = backarr[bright];
+        MyTree root = new MyTree(rootval);
 
+        int index = -1; //【注意】是midarr的index
+        for (int i = mleft; i <= mright; i++) {
+            if (midarr[i] == rootval) {
+                index = i;
+                break;
+            }
+        }
+        int leftsize = index - mleft;
 
-        return null;
+        //因为index是midarr的index，所以只有midarr才可以使用index参数
+        root.left = build(midarr, mleft, index - 1, backarr, bleft, bleft + leftsize - 1);
+        root.right = build(midarr, index + 1, mright, backarr, bleft + leftsize, bright - 1);
+        return root;
     }
 
     public static void main(String[] args) {
