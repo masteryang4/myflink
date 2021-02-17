0450e00a80e90b65ec3a32c7e1c5260f3ca8dce9
masteryang4 <2692474773@qq.com>
2021-02-17 14:47:23 +0800

    xuliehua_fanxuliehua_tree

---
+++ src/main/java/com/ys/algo01/mytree/MakeHeapTest.java
@@ -0,0 +1,21 @@
+package com.ys.algo01.mytree;
+
+import java.util.Arrays;
+
+/**
+ * 构建大顶堆。面试重点。
+ */
+public class MakeHeapTest {
+
+    private static void makemaxheap(int[] arr) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {6, 1, 2, 7, 9, 8};
+
+        System.out.println(Arrays.asList(arr));
+        makemaxheap(arr);
+        System.out.println(Arrays.asList(arr));
+    }
+}
--- src/main/java/com/ys/algo01/mytree/XuliehuaTree.java
+++ src/main/java/com/ys/algo01/mytree/XuliehuaTree.java
@@ -11,6 +11,12 @@ public class XuliehuaTree {
 
     private static List<String> treelist = new LinkedList<>();
 
+    /**
+     * 二叉树 前序序列化 将二叉树序列化成一个列表，空节点按“#”处理
+     *
+     * @param root
+     * @return
+     */
     private static List<String> xuliehua(MyTree root) {
         if (root == null) {
             treelist.add("#");
@@ -24,7 +30,44 @@ public class XuliehuaTree {
         return treelist;
     }
 
+    /**
+     * 反序列化二叉树，将一个字符串（前序）还原回二叉树，【构建】二叉树
+     *
+     * @param str = "1, 2, 4, #, #, 5, #, #, 3, #, 6, #, #"
+     * @return
+     */
+    private static LinkedList<String> mytreelist = new LinkedList<>(); //【重点】LinkedList可以动态变化列表的长度，取一个就少一个
+
+    private static MyTree fanxuliehua(String str) {
+        String[] split = str.split(", "); //【注意】","+" "
+
+        for (String s : split) {
+            mytreelist.add(s);
+        }
+
+        return build(mytreelist);
+    }
+
+    private static MyTree build(LinkedList<String> mytreelist) {
+        if (mytreelist == null) {
+            return null;
+        }
+
+        String first = mytreelist.removeFirst(); //【重点】是removeFirst，使得list是动态的
+        if (first.equals("#")) {
+            return null;
+        }
+        MyTree root = new MyTree(Integer.parseInt(first));
+
+        root.left = build(mytreelist);
+        root.right = build(mytreelist);
+
+        return root;
+    }
+
+
     public static void main(String[] args) {
+        /*
         MyTree t1 = new MyTree(1);
         MyTree t2 = new MyTree(2);
         MyTree t3 = new MyTree(3);
@@ -38,8 +81,17 @@ public class XuliehuaTree {
         t2.right = t5;
         t3.right = t6;
 
-        List<String> mytree = xuliehua(t1);
+         */
 
-        System.out.println(mytree); //[1, 2, 4, #, #, 5, #, #, 3, #, 6, #, #]
+//        List<String> mytree = xuliehua(t1);
+
+//        System.out.println(mytree); //[1, 2, 4, #, #, 5, #, #, 3, #, 6, #, #]
+
+        MyTree root = fanxuliehua("1, 2, 4, #, #, 5, #, #, 3, #, 6, #, #");
+        System.out.println(root.value); //1
+
+        List<String> xuliehua = xuliehua(root);
+        System.out.println(xuliehua); //[1, 2, 4, #, #, 5, #, #, 3, #, 6, #, #]
+
     }
 }
