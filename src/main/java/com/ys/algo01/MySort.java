d2d15ffc61395305c510d7d4093480b99b767a9b
masteryang4 <2692474773@qq.com>
2021-02-21 20:09:01 +0800

    mysort-bubbleSort

--- src/main/java/com/ys/algo01/MySort.java
+++ src/main/java/com/ys/algo01/MySort.java
@@ -4,6 +4,18 @@ import java.util.Arrays;
 
 public class MySort {
 
+    private static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+    }
+
     private static void quickSort(int[] arr, int left, int right) {
         if (left > right) {
             return;
@@ -35,9 +47,10 @@ public class MySort {
         int[] arr = {6, 1, 2, 7, 9, 8};
         System.out.println(Arrays.toString(arr));
 
-
-        quickSort(arr, 0, arr.length - 1);
+//        quickSort(arr, 0, arr.length - 1);
+        bubbleSort(arr);  //[1, 2, 6, 7, 8, 9]
 
         System.out.println(Arrays.toString(arr));
     }
+
 }
