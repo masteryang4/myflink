ac48e63d63231b18db9d3ee3702c3c61c94c89ac
masteryang4 <2692474773@qq.com>
2021-02-22 16:26:02 +0800

    myheapsort_practice

--- src/main/java/com/ys/algo01/QuickSort001.java
+++ src/main/java/com/ys/algo01/QuickSort001.java
@@ -7,8 +7,42 @@ public class QuickSort001 {
         int[] arr = {6, 1, 2, 7, 9, 8};
         System.out.println(Arrays.toString(arr));
 //        quicksort(arr, 0, arr.length - 1);
-        bubblesort(arr);
+//        bubblesort(arr);
+        heapsort(arr);
         System.out.println(Arrays.toString(arr));
+    }
+
+    private static void heapsort(int[] arr) {
+        for (int i = (arr.length - 1) / 2; i >= 0; i--) {
+            makeheap(arr, i, arr.length);
+        }
+//        System.out.println("heap= " + Arrays.toString(arr)); //[9, 7, 8, 6, 1, 2]
+        for (int i = arr.length - 1; i > 0; i--) {
+            int tmp = arr[i];
+            arr[i] = arr[0];
+            arr[0] = tmp;
+
+            makeheap(arr, 0, i);
+        }
+    }
+
+    private static void makeheap(int[] arr, int parent, int length) {
+        int flag = arr[parent];
+        int left = 2 * parent + 1;
+        while (left < length) {
+            int right = left + 1;
+            if (right < length && arr[right] > arr[left]) { //【重点】
+                left = right;
+            }
+            if (arr[left] > flag) {
+                arr[parent] = arr[left];
+                parent = left;
+                left = 2 * left + 1;
+            } else {
+                break;
+            }
+        }
+        arr[parent] = flag;
     }
 
     private static void bubblesort(int[] arr) {
