9a89c6a280ca5d32e449d0982dabe1045dbf72ce
masteryang4 <2692474773@qq.com>
2021-02-22 18:52:45 +0800

    myMergesort_practice

--- src/main/java/com/ys/algo01/QuickSort001.java
+++ src/main/java/com/ys/algo01/QuickSort001.java
@@ -8,10 +8,54 @@ public class QuickSort001 {
         System.out.println(Arrays.toString(arr));
 //        quicksort(arr, 0, arr.length - 1);
 //        bubblesort(arr);
-        heapsort(arr);
+//        heapsort(arr);
+        mergesort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
 
+    /**
+     *
+     * @param arr
+     * @param left
+     * @param right
+     */
+    private static void mergesort(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int mid = (left + right) / 2;
+        mergesort(arr, left, mid);
+        mergesort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int[] resultarr = new int[right - left + 1];
+        int l = left;
+        int m = mid + 1;
+        int k = 0;
+        while (l <= mid && m <= right) {
+            if (arr[l] > arr[m]) {
+                resultarr[k++] = arr[m++];
+            } else {
+                resultarr[k++] = arr[l++];
+            }
+        }
+        while (l <= mid) {
+            resultarr[k++] = arr[l++];
+        }
+        while (m <= right) {
+            resultarr[k++] = arr[m++];
+        }
+        for (int i = 0; i < resultarr.length; i++) {
+            arr[i + left] = resultarr[i];
+        }
+    }
+
+    /**
+     *
+     * @param arr
+     */
     private static void heapsort(int[] arr) {
         for (int i = (arr.length - 1) / 2; i >= 0; i--) {
             makeheap(arr, i, arr.length);
@@ -45,6 +89,10 @@ public class QuickSort001 {
         arr[parent] = flag;
     }
 
+    /**
+     *
+     * @param arr
+     */
     private static void bubblesort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
@@ -57,6 +105,12 @@ public class QuickSort001 {
         }
     }
 
+    /**
+     *
+     * @param arr
+     * @param left
+     * @param right
+     */
     private static void quicksort(int[] arr, int left, int right) {
         if (left > right) { //base case
             return;
