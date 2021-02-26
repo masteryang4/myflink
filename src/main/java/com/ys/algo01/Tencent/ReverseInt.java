378026813878028409068da3152e3ff7a5ac40da
masteryang4 <2692474773@qq.com>
2021-02-26 16:21:16 +0800

    tencentAlgo

---
+++ src/main/java/com/ys/algo01/Tencent/ReverseInt.java
@@ -0,0 +1,21 @@
+package com.ys.algo01.Tencent;
+
+/**
+ * https://mp.weixin.qq.com/s/Cq--AMMDrkV89QXihJWu9w
+ * <p>
+ * 给一个日期：20200202
+ * 观察这个日期，同时满足两个特征：
+ * 1.左右对称
+ * 2.除数字0外，只有一个非零数字
+ * <p>
+ * 编码实现以下两项：
+ * 1.输入任意日期，判断是否满足以上条件，测试用例：20200202，21211212
+ * 2.输出自19700101至今所有符合条件的日期
+ * 参考思路：
+ * //还需要对输入进行校验，是否是正确的日期
+ */
+public class ReverseInt {
+    public static void main(String[] args) {
+
+    }
+}
