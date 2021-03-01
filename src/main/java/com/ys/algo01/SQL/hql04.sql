0059a553c1a99c50cee24f67d8a693c7fdcc6617
masteryang4 <2692474773@qq.com>
2021-03-01 17:35:43 +0800

    hql04-think

--- src/main/java/com/ys/algo01/SQL/hql04.sql
+++ src/main/java/com/ys/algo01/SQL/hql04.sql
@@ -20,12 +20,37 @@ where date_format(dt, 'yyyy') = '2017'
 group by date_format(dt, 'yyyy-MM');
 
 -- 2）给出2017年11月的新客数(指在11月才有第一笔订单)
---【重点】
+--【重点】，但是是错误答案
 select count(user_id)
 from order_tab
 group by user_id
 having date_format(min(dt), 'yyyy-MM') = '2017-11';
 
+--【正确答案】
+--查询每个user_id的个数
+select count(user_id)
+from order_tab
+group by user_id;
+
+--查询每个user_id，对应的所有日期的最小值，且最小值为‘2017年11月’
+--having的作用：结合group by，对“上一步”查询的结果进行过滤
+select count(user_id)
+from order_tab
+group by user_id
+having date_format(min(dt), 'yyyy-MM') = '2017-11';
+
+--结果
+SELECT count(1)
+from (
+         select count(user_id)
+         from order_tab
+         group by user_id
+         having date_format(min(dt), 'yyyy-MM') = '2017-11'
+     ) as a
+--having date_format(min(dt), '%Y-%m') = '2017-11'  mysql版本
+-- having的作用：结合group by，对查询的结果进行过滤
+
+--========================================================================
 --【注意】where后里面不能嵌套min
 
 -- select count(user_id)
