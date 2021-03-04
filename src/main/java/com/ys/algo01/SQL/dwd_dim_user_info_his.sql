4b99cc596c0a7d77f321cff2c7e7524212227793
masteryang4 <2692474773@qq.com>
2021-03-04 23:41:28 +0800

    user-info-his-sql

--- src/main/java/com/ys/algo01/SQL/dwd_dim_user_info_his.sql
+++ src/main/java/com/ys/algo01/SQL/dwd_dim_user_info_his.sql
@@ -11,7 +11,7 @@ create external table dwd_dim_user_info_his
     `operate_time` string COMMENT '操作时间',
     `start_date`   string COMMENT '有效开始日期',
     `end_date`     string COMMENT '有效结束日期'
-) COMMENT '订单拉链表'
+) COMMENT '用户拉链表'
     stored as parquet
     location '/warehouse/gmall/dwd/dwd_dim_user_info_his/'
     tblproperties ("parquet.compression" = "lzo");
